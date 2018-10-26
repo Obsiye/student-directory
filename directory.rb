8ec3d8af731b13e0e715aa8980fb8d75922b08da
Obsiye <mrabdirizak@gmail.com>
2018-10-26 13:17:44 +0100

    Add more conditionals
    Print student with name less than 12 characters long
    AND
    Request for a character between [A-Za-z] and only print students with name beginning with inputted character.

--- directory.rb
+++ directory.rb
@@ -3,12 +3,28 @@ def print_header
  puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    car = name_begins_with
+  n = 0
+
+  while n < students.length
+    if students[n][:name].start_with?(car) && students[n][:name].length < 12
+      puts "#{n + 1}. #{students[n][:name]} (#{students[n][:cohort]} cohort). Born in #{students[n][:birth_country]}"
+    end
+    n += 1
   end
+
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
+end
+
+def name_begins_with
+  
+ puts "Print only names begining with character...\nPlease enter a character between A-Z"
+
+ character = gets.chomp.upcase
+ character
 end
 
 def input_students
@@ -21,7 +37,7 @@ def input_students
 
   while !name.empty?
 
-    students << {name: name, cohort: :november}
+    students << {name: name.capitalize, cohort: :november, hobbie: "Tennis" , birth_country: :Britain}
     puts "Now we have #{students.count} students"
 
     name = gets.chomp
